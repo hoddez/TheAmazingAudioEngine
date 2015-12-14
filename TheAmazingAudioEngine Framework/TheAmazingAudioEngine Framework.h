1f439e6a5c68b3dc9197385eba44b3a420222ba1
hoddez <thoddes@gmail.com>
2015-12-13 17:39:22 -0800

    New Framework Target to support Carthage. This might be pointless once swift package managers become more of a thing but for now I think it makes life easier when you have your own swift Modules that depend on TheAmazingAudioEngine.

---
+++ TheAmazingAudioEngine Framework/TheAmazingAudioEngine Framework.h
@@ -0,0 +1,19 @@
+//
+//  TheAmazingAudioEngine Framework.h
+//  TheAmazingAudioEngine Framework
+//
+//  Created by Thomas Hoddes on 2015-12-13.
+//  Copyright © 2015 A Tasty Pixel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TheAmazingAudioEngine Framework.
+FOUNDATION_EXPORT double TheAmazingAudioEngine_FrameworkVersionNumber;
+
+//! Project version string for TheAmazingAudioEngine Framework.
+FOUNDATION_EXPORT const unsigned char TheAmazingAudioEngine_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TheAmazingAudioEngine_Framework/PublicHeader.h>
+
+#import "TheAmazingAudioEngine.h"
